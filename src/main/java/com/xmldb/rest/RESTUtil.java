7426d8c6fdfde5c8fb2663433570e94d4df53cf2
Nikola Mihalek <nikola.mihalek@gmail.com>
2015-03-15 00:27:02 +0100

    adding the connector feature

---
+++ src/main/java/com/xmldb/rest/RESTUtil.java
@@ -0,0 +1,202 @@
+package com.xmldb.rest;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.apache.commons.io.IOUtils;
+
+import com.xmldb.rest.misc.RequestMethod;
+
+/**
+ * Klasa demonstrira upotrebu REST API-a BaseX XML baze podataka.
+ * Sadrzi set reusable CRUD operacija, sa primerom njihove upotrebe. 
+ * 
+ * @author Igor Cverdelj-Fogarasi
+ *
+ */
+public class RESTUtil {
+
+	public static final String REST_URL = ResourceBundle.getBundle("basex").getString("rest.url");
+
+	public static int createSchema(String schemaName) throws Exception {
+		System.out.println("=== PUT: create a new database: " + schemaName + " ===");
+		URL url = new URL(REST_URL + schemaName);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod(RequestMethod.PUT);
+		int responseCode = printResponse(conn);
+		conn.disconnect();
+		return responseCode;
+	}
+	
+	public static int createResource(String schemaName, String resourceId, InputStream resource) throws Exception {
+		System.out.println("=== PUT: create a new resource: " + resourceId + " in database: " + schemaName + " ===");
+		URL url = new URL(REST_URL + schemaName + "/" + resourceId + "?chop=false");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod(RequestMethod.PUT);
+		
+		/* Preuzimanje output stream-a iz otvorene konekcije */
+		OutputStream out = new BufferedOutputStream(conn.getOutputStream());
+
+		/* Slanje podataka kroz stream */
+		System.out.println("\n* Send document...");
+		IOUtils.copy(resource, out);
+		IOUtils.closeQuietly(resource);
+		IOUtils.closeQuietly(out);
+		
+		int responseCode = printResponse(conn);
+		conn.disconnect();
+		return responseCode;
+	}
+	
+	public static int updateResource(String schemaName, String resourceId, InputStream resource) throws Exception {
+		return createResource(schemaName, resourceId, resource);
+	}
+	
+	public static InputStream retrieveSpecificResource(String fileName, String schemaName) throws Exception {
+		String functionName="doc(\"";
+		String functionEnd="\")";
+		if(fileName.lastIndexOf(".")<0) {
+			fileName+=".xml";
+		}
+		String query = functionName+schemaName+"/"+fileName+functionEnd;
+		//return retrieveSpecificResourceStandalone(query, schemaName, "UTF-8", true, true);
+		return retrieveSpecificResourceStandalone(query, schemaName, "UTF-8", true, false);
+		
+	}
+	
+	private static InputStream retrieveSpecificResourceStandalone(String query, String schemaName, String encoding, boolean omitXmlDeclaration, boolean standalone) throws Exception {
+		System.out.println("=== GET: execute a query: " + query + " ===");
+		
+		StringBuilder builder = new StringBuilder(REST_URL);
+		builder.append(schemaName);
+		builder.append("?query=");
+		builder.append(query.replace(" ", "+"));
+		builder.append("&encoding=");
+		builder.append(encoding);
+		builder.append("&standolone=");
+		builder.append("standalone");
+		builder.append("&omit-xml-declaration=");
+		builder.append(omitXmlDeclaration);
+
+		URL url = new URL(builder.substring(0));
+		
+		System.out.println(url);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+		/* Response kod vracen od strane servera */
+		int responseCode = printResponse(conn);
+
+		/* Ako je sve proslo kako treba... */
+		if (responseCode == HttpURLConnection.HTTP_OK) 
+			return conn.getInputStream();
+		
+		conn.disconnect();
+		return null;
+	}
+	
+	public static InputStream retrieveResource(String query, String schemaName, String method) throws Exception {
+		if (method == RequestMethod.GET)
+			return retrieveResource(query, schemaName, "UTF-8", true);
+		else if (method == RequestMethod.POST)
+			return retrieveResourcePost(query, schemaName);
+		return null;
+	}
+	
+	public static InputStream retrieveResourcePost(String xquery, String schemaName) throws Exception {
+		System.out.println("=== POST: execute a query ===");
+		URL url = new URL(REST_URL + schemaName);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod(RequestMethod.POST);
+		conn.setRequestProperty("Content-Type", "application/query+xml");
+		OutputStream out = conn.getOutputStream();
+		out.write(xquery.getBytes("UTF-8"));
+		out.close();
+
+		/* Response kod vracen od strane servera */
+		int responseCode = printResponse(conn);
+
+		/* Ako je sve proslo kako treba... */
+		if (responseCode == HttpURLConnection.HTTP_OK) 
+			return conn.getInputStream();
+		
+		conn.disconnect();
+		return null;
+	}
+	
+	public static InputStream retrieveResource(String query, String schemaName, String encoding, boolean wrap) throws Exception {
+		System.out.println("=== GET: execute a query: " + query + " ===");
+		
+		StringBuilder builder = new StringBuilder(REST_URL);
+		builder.append(schemaName);
+		builder.append("?query=");
+		builder.append(query.replace(" ", "+"));
+		builder.append("&encoding=");
+		builder.append(encoding);
+		if (wrap) builder.append("&wrap=yes");
+
+		URL url = new URL(builder.substring(0));
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+		/* Response kod vracen od strane servera */
+		int responseCode = printResponse(conn);
+
+		/* Ako je sve proslo kako treba... */
+		if (responseCode == HttpURLConnection.HTTP_OK) 
+			return conn.getInputStream();
+		
+		conn.disconnect();
+		return null;
+	}
+
+	public static int deleteResource(String schemaName, String resourceId) throws Exception {
+		if (resourceId != null && !resourceId.equals(""))
+			System.out.println("=== DELETE: delete resource: " + resourceId + " from database: " + schemaName + " ===");
+		else 
+			System.out.println("=== DELETE: delete existing database: " + schemaName + " ===");
+		
+		URL url = new URL(REST_URL + schemaName + "/" + resourceId);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod(RequestMethod.DELETE);
+		int responseCode = printResponse(conn);
+		conn.disconnect();
+		return responseCode;
+	}
+	
+	public static int dropSchema(String schemaName) throws Exception {
+		return deleteResource(schemaName, "");
+	}
+
+	public static int printResponse(HttpURLConnection conn) throws Exception {
+		int responseCode = conn.getResponseCode();
+		String message = conn.getResponseMessage();
+		System.out.println("\n* HTTP response: " + responseCode + " (" + message + ')');
+		return responseCode;
+	}
+	
+	public static void printStream(InputStream input) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(input));
+		for (String line; (line = br.readLine()) != null;) {
+			System.out.println(line);
+		}
+	}
+	
+	public static boolean isRunning() throws Exception {
+		try {
+			((HttpURLConnection) new URL(REST_URL).openConnection()).getResponseCode();
+		} catch (ConnectException e) {
+			return false;
+		}
+		return true;
+	}
+	
+}
